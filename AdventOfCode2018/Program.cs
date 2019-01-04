1a7282d11a8cb82f7145b72064fdab5b71109da1
Athellas <mm.krupinski@gmail.com>
2019-01-05 00:43:22 +0100

    Update Program.cs
    day 2 puzzle 1`

--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AdventOfCode2018
 {
@@ -10,6 +11,64 @@ namespace AdventOfCode2018
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Mic check check check");
+            Console.WriteLine("");
+            //Console.WriteLine("IO test");
+            //List<string> input = ConvertPuzzleInputToListString(@"C:\_aoc2018\day2puzzle1.txt");
+            //foreach(string s in input)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            //Console.ReadLine();
+            Console.WriteLine("Day 2 Puzzle 2:");
+            Console.WriteLine(Day2Puzzle1(@"C:\_aoc2018\day2puzzle1.txt"));
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        static string Day2Puzzle1(string path)
+        {
+            List<string> puzzleInput = new List<string> { };
+            puzzleInput = ConvertPuzzleInputToListString(path);
+            int numberOfTwos=0;
+            int numberOfThrees=0;
+            //char[] lineChars = new char[] { }; 
+            foreach (string line in puzzleInput)
+            {
+                char[] lineChars= line.ToCharArray(0, line.Length);
+                Dictionary<char, int> hashmap = new Dictionary<char, int>();
+                
+                for(int i = 0; i<line.Length; i++)
+                {
+                    if (hashmap.ContainsKey(lineChars[i]))
+                    {
+                        int get = hashmap[lineChars[i]];
+                        hashmap[lineChars[i]] = get + 1;
+                    }
+                    else
+                    {
+                        hashmap[lineChars[i]] = 1;
+                    }
+                }
+
+                if (hashmap.ContainsValue(2))
+                    { numberOfTwos++; }
+                if (hashmap.ContainsValue(3))
+                    { numberOfThrees++; }
+            }
+
+            return (numberOfTwos*numberOfThrees).ToString();
+        }
+
+        static List<string> ConvertPuzzleInputToListString(string path)
+        {
+            var lines = File.ReadAllLines(path);
+            List<string> puzzleInputListString = new List<string> { };
+            foreach (var line in lines)
+            {
+                puzzleInputListString.Add(line);
+            }
+            return puzzleInputListString;
         }
     }
 }
