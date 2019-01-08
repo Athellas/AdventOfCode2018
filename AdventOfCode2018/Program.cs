d02b390777e5050800c096f423a09bbdb08ef61d
Athellas <mm.krupinski@gmail.com>
2019-01-08 17:07:30 +0100

    Update Program.cs
    Day 3 puzzle 1

--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2018
 {
@@ -20,13 +21,18 @@ namespace AdventOfCode2018
             //    Console.WriteLine(s);
             //}
             //Console.ReadLine();
-            Console.WriteLine("Day 2 Puzzle 1:");
-            Console.WriteLine(Day2Puzzle1(@"C:\_aoc2018\day2puzzle1.txt"));
-            Console.WriteLine();
-            Console.ReadLine();
+            //Console.WriteLine("Day 2 Puzzle 1:");
+            //Console.WriteLine(Day2Puzzle1(@"C:\_aoc2018\day2puzzle1.txt"));
+            //Console.WriteLine();
+            //Console.ReadLine();
 
-            Console.WriteLine("Day 2 Puzzle 2:");
-            Console.WriteLine(Day2Puzzle2(@"C:\_aoc2018\day2puzzle1.txt"));
+            //Console.WriteLine("Day 2 Puzzle 2:");
+            //Console.WriteLine(Day2Puzzle2(@"C:\_aoc2018\day2puzzle1.txt"));
+            //Console.WriteLine();
+            //Console.ReadLine();
+
+            Console.WriteLine("Day 3 Puzzle 1: ");
+            Console.WriteLine(Day3Puzzle1(@"C:\_aoc2018\day3puzzle1.txt"));
             Console.WriteLine();
             Console.ReadLine();
         }
@@ -99,6 +105,70 @@ namespace AdventOfCode2018
                     finalWord.Add(puzzleInput[candidateIndex[0]][i]);
             }            
             return string.Concat(finalWord);
+        }
+
+        static string Day3Puzzle1(string path)
+        {
+            List<string> puzzleInput = new List<string> { };
+            puzzleInput = ConvertPuzzleInputToListString(path);
+
+            int[,] fabric = new int[1000, 1000];
+
+            // #1 @ 286,440: 19x24
+
+            List<int> positionX = new List<int>(new int[puzzleInput.Count]);
+            string patternPositionX = @"(\d\d\d\,)| (\d\d\,)| (\d\,)";
+            for (int i = 0; i < puzzleInput.Count; i++)
+            {
+                positionX[i] = Int32.Parse((Regex.Match(puzzleInput[i], patternPositionX).Value).TrimEnd(','));
+                Console.WriteLine(positionX[i].ToString());                
+            }
+
+            List<int> positionY = new List<int>(new int[puzzleInput.Count]);
+            string patternPositionY = @"(\,\d\d\d)|(\,\d\d)|(\,\d)";
+            for (int i = 0; i < puzzleInput.Count; i++)
+            {
+                positionY[i] = Int32.Parse((Regex.Match(puzzleInput[i], patternPositionY).Value).TrimStart(','));
+                Console.WriteLine(positionY[i].ToString());
+            }
+
+            List<int> sizeX = new List<int>(new int[puzzleInput.Count]);
+            string patternSizeX = @"(\d\dx)|(\dx)";
+            for (int i = 0; i < puzzleInput.Count; i++)
+            {
+                sizeX[i] = Int32.Parse((Regex.Match(puzzleInput[i], patternSizeX).Value).TrimEnd('x'));
+                Console.WriteLine(sizeX[i].ToString());
+            }
+
+            List<int> sizeY = new List<int>(new int[puzzleInput.Count]);
+            string patternSizeY = @"(x\d\d)|(x\d)";
+            for (int i = 0; i < puzzleInput.Count; i++)
+            {
+                sizeY[i] = Int32.Parse((Regex.Match(puzzleInput[i], patternSizeY).Value).TrimStart('x'));
+                Console.WriteLine(sizeY[i].ToString());
+            }
+
+            for (int i=0; i < puzzleInput.Count; i++)
+            {
+                for (int x = 0; x < sizeX[i]; x++ )
+                {
+                    for (int y = 0; y < sizeY[i]; y++)
+                    {
+                        fabric[positionX[i] + x, positionY[i] + y]++;
+                    }
+                }
+            }
+
+            int result=0;
+            foreach (int z in fabric)
+            {
+                if (z > 1)
+                {
+                    result++;
+                }
+            }
+            
+            return result.ToString();
         }
 
         static List<string> ConvertPuzzleInputToListString(string path)
